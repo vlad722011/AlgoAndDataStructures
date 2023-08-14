349d37ee810c2049075bc4aa0a1b193ce4783e40
vlad722011 <vlad722011@yandex.ru>
2023-08-14 22:47:05 +0700

    Добавлены варианты реализации сортировки массива подсчетом (для положительных чисел).

---
+++ src/main/java/Seminar_2/SortingByCounting.java
@@ -0,0 +1,60 @@
+package Seminar_2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortingByCounting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите длину входящего массива: ");
+        int arraySize = scanner.nextInt();
+        System.out.println("Введите элементы массива : ");
+        int[] inArray = new int[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            inArray[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(inArray));
+        System.out.println("Сортируем массив методом сортировки подсчетом): ");
+        System.out.println(Arrays.toString(sortArrayByCountingMethod(inArray)));
+    }
+
+
+    private static int [] sortArrayByCountingMethod(int[] array) {
+
+        //Найдём максимальное число в массиве
+
+        int maxElementInArray = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxElementInArray) {
+                maxElementInArray = array[i];
+            }
+
+        }
+        //Создадим новый массив длиной, равной значению максимального элемента входящего массива,
+        // увеличенному на единицу. Созданный массив по умолчанию наполнен нулями.
+
+        int[] tempArray = new int[maxElementInArray + 1];
+
+        for (int i = 0; i < array.length; i++) {
+            int index = array[i];
+            tempArray[index] = tempArray[index] + 1;
+        }
+
+        // Отсортируем входящий массив.
+
+        int value = Integer.MAX_VALUE;
+        int index = 0;
+        for (int i = 0; i < tempArray.length; i++) {
+            if (tempArray[i] != 0) {
+                value = i;
+                int count = tempArray[i];
+                while (count > 0){
+                    array[index] = value;
+                    index++;
+                    count --;
+                }
+            }
+        }
+        return array;
+    }
+}
---
+++ src/main/java/Seminar_2/SortingByCounting2.java
@@ -0,0 +1,50 @@
+package Seminar_2;
+
+import java.util.Arrays;
+
+public class SortingByCounting2 {
+    public static void main(String[] args) {
+        int[] arr = {19, 234, 714, 2000, 7, 2, 5, 4, 3, 2, 543, 98, 168, 2, 657, 345};
+        System.out.println("Входящий массив");
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("Сортируем массив методом сортировки подсчетом): ");
+        System.out.println(Arrays.toString(sortArrayByCountingMethod(arr)));
+    }
+
+
+    private static int[] sortArrayByCountingMethod(int[] array) {
+
+        //Найдём максимальное число в массиве
+        int maxElementInArray = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxElementInArray) {
+                maxElementInArray = array[i];
+            }
+        }
+
+        //Создадим новый массив длиной, равной значению максимального элемента входящего массива,
+        // увеличенному на единицу. Созданный массив по умолчанию наполнен нулями.
+        int[] tempArray = new int[maxElementInArray + 1];
+        int[] tempArray1 = new int[maxElementInArray + 1];
+
+        //Запишем в него количество вхождений каждого элемента поиндексно.
+        // Первый вариант создания дополнительного массива.
+        for (int value : array) {
+            ++tempArray[value];
+        }
+        // Создаем тот же дополнительный массив, более читаемой записью.
+        for (int value : array) {
+            tempArray1[value] = tempArray1[value] + 1;
+        }
+
+        // Сортируем массив
+        int b = 0;
+        for (int i = 0; i < maxElementInArray + 1; ++i) {
+            for (int j = 0; j < tempArray[i]; ++j) {
+                array[b++] = i;
+            }
+        }
+        return array;
+    }
+}
