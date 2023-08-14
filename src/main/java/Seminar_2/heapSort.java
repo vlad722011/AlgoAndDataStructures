b8687af0f129d644f52320894dd8f847cce8bfab
vlad722011 <vlad722011@yandex.ru>
2023-08-14 18:16:25 +0700

    Initial commit

---
+++ src/main/java/Seminar_2/heapSort.java
@@ -0,0 +1,55 @@
+package Seminar_2;
+import java.util.Arrays;
+
+public class heapSort {
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 2, 3, 5, 6, 6, 7, 957, -112, 817, 0, 94};
+        System.out.println("Входящий массив");
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Отсортированный массив");
+        System.out.println(Arrays.toString(sort(arr)));
+    }
+    public static int[] sort(int[] array) {
+        // построение кучи (перегруппируем массив)
+        for (int i = array.length / 2 - 1; i >= 0; i--) {
+            heapify(array, array.length, i);
+        }
+        // один за другим извлекаем элементы из кучи
+        for (int i = array.length - 1; i >= 0; i--) {
+            // перемещаем текущий корень в конец
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            // вызываем процедуру heapify на уменьшенной куче
+            heapify(array, i, 0);
+        }
+        return array;
+    }
+
+    public static void heapify(int[] array, int heapSize, int rootIndex) {
+
+        // инициализируем наибольший элемент как корень
+        int largest = rootIndex;
+        // левый потомок корня
+        int leftChild = rootIndex * 2 + 1;
+        // правый потомок корня
+        int rightChild = rootIndex * 2 + 2;
+        // если левый потомок больше корня
+        if (leftChild < heapSize && array[leftChild] > array[largest]) {
+            largest = leftChild;
+        }
+        // если правый потомок больше, чем самый большой элемент на данный момент
+        if (rightChild < heapSize && array[rightChild] > array[largest]) {
+            largest = rightChild;
+        }
+
+        // если самый большой элемент не корень
+        if (largest != rootIndex) {
+            int temp = array[rootIndex];
+            array[rootIndex] = array[largest];
+            array[largest] = temp;
+            // рекурсивно преобразуем в двоичную кучу затронутое поддерево
+            heapify(array, heapSize, largest);
+        }
+    }
+}
---
+++ src/main/java/Seminar_2/heapSort3.java
@@ -0,0 +1,77 @@
+package Seminar_2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class heapSort3 {
+
+    private static int heapSize;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите длину входящего массива: ");
+        int arraySize = scanner.nextInt();
+        System.out.println("Введите элементы массива : ");
+        int[] inArray = new int[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            inArray[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(inArray));
+
+
+        System.out.println("Сортируем массив методом сортировки кучей (наглядная демонстрация процесса, пошагово): ");
+        sort(inArray);
+
+
+    }
+
+    public static void sort(int[] arr) {
+        buildHeap(arr);
+        while (heapSize > 1) {
+            swap(arr, 0, heapSize - 1);
+            heapSize--;
+            heapify(arr, 0);
+        }
+    }
+
+    public static void buildHeap(int[] array) {
+        heapSize = array.length;
+        for (int i = array.length / 2; i >= 0; i--) {
+            heapify(array, i);
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void heapify(int[] a, int rootIndex) {
+        int l = left(rootIndex);
+        int r = right(rootIndex);
+        int largest = rootIndex;
+        if (l < heapSize && a[rootIndex] < a[l]) {
+            largest = l;
+        }
+        if (r < heapSize && a[largest] < a[r]) {
+            largest = r;
+        }
+        if (rootIndex != largest) {
+            swap(a, rootIndex, largest);
+            heapify(a, largest);
+        }
+    }
+
+    private static int right(int i) {
+        return 2 * i + 2;
+    }
+
+    private static int left(int i) {
+        return 2 * i + 1;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        System.out.println(Arrays.toString(array));
+    }
+}
+
+
