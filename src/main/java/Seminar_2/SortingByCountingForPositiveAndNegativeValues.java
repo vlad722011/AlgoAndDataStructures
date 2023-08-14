e8812db0742e2deba6332480019892ca8ff42d34
vlad722011 <vlad722011@yandex.ru>
2023-08-14 23:48:05 +0700

    Изменил метод майн. Входящий массив задает пользователь. Возможны на вход любые значения и любая длинна входящего массива.

--- src/main/java/Seminar_2/SortingByCountingForPositiveAndNegativeValues.java
+++ src/main/java/Seminar_2/SortingByCountingForPositiveAndNegativeValues.java
@@ -1,16 +1,21 @@
 package Seminar_2;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class SortingByCountingForPositiveAndNegativeValues {
     public static void main(String[] args) {
-        int[] arr = {3, 5, -4, -6, 4, 3, 3, -6};
-        System.out.println("Входящий массив");
-        System.out.println(Arrays.toString(arr));
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите длину входящего массива: ");
+        int arraySize = scanner.nextInt();
+        System.out.println("Введите элементы массива : ");
+        int[] inArray = new int[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            inArray[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(inArray));
         System.out.println("Сортируем массив методом сортировки подсчетом): ");
-        System.out.println(Arrays.toString(sortArrayByCountingMethod(arr)));
-
+        System.out.println(Arrays.toString(sortArrayByCountingMethod(inArray)));
     }
 
     private static int [] sortArrayByCountingMethod(int[] array) {
